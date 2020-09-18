6db378715a8403cba85eaf83c3f9cbc708da5aac
Gabriela-Berenice <70357175+Gabriela-Berenice@users.noreply.github.com>
2020-09-18 17:29:20 -0600

    Update ejer2.py

--- IS/Actividad 2/ejer2.py
+++ IS/Actividad 2/ejer2.py
@@ -5,6 +5,9 @@ import time
 books = np.genfromtxt('libros_24_meses.txt')
 lista = np.genfromtxt('catalogo_libros.txt')
 
+inicio = time.time()
+
 #Filtro
 filtro = list(filter(lambda n: n in books, lista))
 print(len(filtro))
+print('Tiempo: {} segundos'.format(time.time() - inicio))
