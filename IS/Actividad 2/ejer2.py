12d66cc784b9e133377a8184e564dcd664f89334
Gabriela-Berenice <70357175+Gabriela-Berenice@users.noreply.github.com>
2020-09-18 17:23:51 -0600

    Update ejer2.py

--- IS/Actividad 2/ejer2.py
+++ IS/Actividad 2/ejer2.py
@@ -1,8 +1,10 @@
 import numpy as np
 import time 
 
+#Se importa los archivos de texto
 books = np.genfromtxt('libros_24_meses.txt')
 lista = np.genfromtxt('catalogo_libros.txt')
 
+#Filtro
 filtro = list(filter(lambda n: n in books, lista))
 print(len(filtro))
