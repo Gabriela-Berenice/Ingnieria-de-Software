7e2b8a60d6d43ecd76328b5b6f8d2cfe0140cc8c
Gabriela-Berenice <70357175+Gabriela-Berenice@users.noreply.github.com>
2020-09-21 14:43:53 -0600

    Update Asignacion.py

--- IS/Actividad3/Asignacion.py
+++ IS/Actividad3/Asignacion.py
@@ -1,14 +1,12 @@
 import numpy as np
 import time 
 
-# Se Importa el documento y se lamacena en una variable
 with open('costos.txt') as c:
     lista_costos = c.read().split('\n')
 
 inicio = time.time()
 menor = []
 
-#convertir a int la lista txt
 costos = map(int, lista_costos)
 
 #comparacion de los numeros iguales o menores a 25
