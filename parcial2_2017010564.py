0028bef44ccdf2e9722c9efe42890f3667a372c8
Gabriela-Berenice <70357175+Gabriela-Berenice@users.noreply.github.com>
2020-10-08 16:25:15 -0600

    Add files via upload

---
+++ parcial2_2017010564.py
@@ -0,0 +1,14 @@
+import random
+import numpy as np
+import time
+
+puntos = np.random.normal(500,10000000,30)
+
+inicio = time.time()
+#comparacion menores o iguales a 500000
+
+lista_puntos = puntos[puntos < 500000]
+total_puntos = np.sum(lista_puntos)
+
+print('Monto total de la suma de los numeros menores a 500,000 es $', total_puntos)
+print('Tiempo: {} segundos'.format(time.time() - inicio))
