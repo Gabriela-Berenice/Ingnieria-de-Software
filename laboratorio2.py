dd9179aecbdb065963bfffe272bff63efa641ff9
Gabriela-Berenice <70357175+Gabriela-Berenice@users.noreply.github.com>
2020-10-07 20:45:47 -0600

    Update laboratorio2.py

--- laboratorio2.py
+++ laboratorio2.py
@@ -1,3 +1,7 @@
 
-for i in range(500,10000,30):
-    print(i)
+for i in range(500,10000000,30):
+    puntos = i
+lista_puntos = puntos[puntos < 500000]
+suma_total = np.sum(lista_puntos)
+
+print(suma_total)
